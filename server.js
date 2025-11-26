e1ccf2f02e617bdacc16c64e360c731219aaad8b
Mr-SK534 <ksharad534@gmail.com>
2025-11-26 21:56:51 +0530

    backend-update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*", methods: ["GET", "POST"] }
 });
